686715018bf2f2416709208becafb2019c85d082
Anuj Gautam <anuj_gtm1@hotmail.com>
2016-04-07 09:14:28 +0545

    Better looking pieces

--- showFEN.py
+++ showFEN.py
@@ -1,10 +1,9 @@
 import graphics as g
-pattern = "PBQpbqNRKnrk"
+pattern = "KkQqBbNnRrPp"
 fen = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
 
-p = g.assignPieces(g.getPieces("p.png",50,50),pattern)
+p = g.assignPieces(g.getPieces("Pieces.png", 50, 50), pattern)
 b = g.drawBoard()
 f = g.readFEN(fen)
 
-g.setBoard(b,p,f)
-
+g.setBoard(b, p, f)
